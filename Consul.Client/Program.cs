75e5af9143479a1e69129eff23455464d4690ac0
颜超 李 <liyanchao0624@hotmail.com>
2019-12-03 19:43:02 +0800

    consul

---
+++ Consul.Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using Consul.Server;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Consul.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceName = "grpctest";
+            var consulClient = new ConsulClient(c => c.Address = new Uri("http://localhost:8500"));
+            var services = await consulClient.Catalog.Service(serviceName);
+            if (services.Response.Length == 0)
+            {
+                throw new Exception($"未发现服务 {serviceName}");
+            }
+
+            var service = services.Response[0];
+            var address = $"http://{service.ServiceAddress}:{service.ServicePort}";
+
+            Console.WriteLine($"获取服务地址成功：{address}");
+
+            //启用通过http使用http2.0
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress(address);
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(new HelloRequest { Name = "Jack" });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Consul.Server/Services/HealthCheckService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Grpc.Health.V1;
+using Grpc.HealthCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consul.Server
+{
+    public class HealthCheckService : HealthServiceImpl
+    {
+        public override Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HealthCheckResponse() { Status = HealthCheckResponse.Types.ServingStatus.Serving });
+        }
+
+        public override async Task Watch(HealthCheckRequest request, IServerStreamWriter<HealthCheckResponse> responseStream, ServerCallContext context)
+        {
+            await responseStream.WriteAsync(new HealthCheckResponse()
+            { Status = HealthCheckResponse.Types.ServingStatus.Serving });
+        }
+    }
+}
